5aa268025ae7433c345e8c70708725368adfedf6
Nahom Haile <nahom9688@gmail.com>
2021-08-10 19:33:39 -0400

    Partial code for Board.js

--- components/Board.js
+++ components/Board.js
@@ -1,59 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import Chess from '../public/js/chess'
 import Square from './Square'
 import styles from '../styles/Demo.module.css'
+import { render } from 'react-dom';
 
 const chess = new Chess();
 
-export default function Board() {
-    var selected = Array.from(Array(8), () => new Array(8).fill(false));
+export default class Board extends Component {
 
-    console.table(selected);
-
-    var board = chess.Board();
-    var uiBoard = new Array(8);
-
-    for (let row = 0; row < uiBoard.length; row++) {
-        uiBoard[row] = new Array(8);
-
-        for (let col = 0; col < uiBoard.length; col++) {
-            let piece = board[row][col];
-            let imagePath;
-
-            if (piece !== ' ') {
-                if (piece === piece.toLowerCase()) {
-                    imagePath = "/images/black/" + piece;
-                }
-                else {
-                    imagePath = "/images/white/" + piece;
-                }
-            }
-
-            uiBoard[row][col] = 
-                                <Square 
-                                    style = {{
-                                        backgroundColor: selected[row][col] ? '#E8F0AF' : (row % 2) === (col % 2) ? '#f7fee7' : '#94b584',
-                                        backgroundImage: piece === ' ' ? 'none' : `url(${imagePath}.svg)`
-                                    }}
-                                    key={row.toString() + col}
-                                    onClick = {(e) => setAsSelected(e, selected, row, col)}
-                                     />;
-        }
+    state = {
+        squareSelected: String,
+        selected: Array.from(Array(8), () => new Array(8).fill(false)),
+        board: chess.Board(),
+        uiBoard: new Array(8)
     }
 
-    return (
-        <div className={styles.center}>
-            <div className={styles.board}>
-                {uiBoard}
-            </div>
-            <a href="http://localhost:3000/">Go Back</a>
-        </div>
-    );
-}
+    setAsSelected = (e, selected, row, col) => {
+        selected[row][col] = true;
+        console.log("attempted");
+        console.table(selected);
+        console.log(e);
+    }
 
-function setAsSelected(e, selected, row, col) {
-    selected[row][col] = true;
-    console.log("attempted");
-    console.table(selected);
-    console.log(e);
+
+    setUpBoard = () => {
+        for (let row = 0; row < this.state.uiBoard.length; row++) {
+            this.state.uiBoard[row] = new Array(8);
+
+            for (let col = 0; col < this.state.uiBoard.length; col++) {
+                let piece = this.state.board[row][col];
+                let imagePath;
+
+                if (piece !== ' ') {
+                    if (piece === piece.toLowerCase()) {
+                        imagePath = "/images/black/" + piece;
+                    } else {
+                        imagePath = "/images/white/" + piece;
+                    }
+                }
+
+                this.state.uiBoard[row][col] = <
+                    Square
+                style = {
+                    {
+                        backgroundColor: this.state.selected[row][col] ? '#E8F0AF' : (row % 2) === (col % 2) ? '#f7fee7' : '#94b584',
+                        backgroundImage: piece === ' ' ? 'none' : `url(${imagePath}.svg)`
+                    }
+                }
+                key = { row.toString() + col }
+                onClick = {(e)=>this.setAsSelected(e, selected, row, col)}
+                />;
+            }
+        }
+
+    }
+
+    render() {
+        let {uiBoard} = this.state;
+        return (
+            <div className = { styles.center } >
+                {this.setUpBoard()}
+                <div className = { styles.board } > 
+                    { uiBoard } 
+                </div> 
+                <a href = "http://localhost:3000/" > Go Back </a> 
+            </div >        
+        )
+    }
+
 }
